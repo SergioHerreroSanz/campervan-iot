d2a978e9898c3975d370df1865ccf98a48ef574c
Sergio Herrero <movilsergio2000@gmail.com>
2025-07-23 23:28:39 +0200

    Modify auto curves

--- ventilation-fan/src/fanModeAuto.h
+++ ventilation-fan/src/fanModeAuto.h
@@ -3,20 +3,26 @@
 #include <Arduino.h>
 #include <tempHistory.h>
 
+int calculatePower(float a, float b, float t0, float temp)
+{
+    if (temp < t0)
+        return 0;
+
+    float pwr = a * (exp(b * (temp - t0)) - 1);
+
+        if (pwr > 100)
+        return 100;
+    if (pwr < 0)
+        return 0;
+    return pwr;
+}
+
 float updateFanAutoLowPower()
 {
-    float temp = getLastTemp();
-    int midpoint = 38;
-    float steepness = 0.25;
-    float power = 1 / (1 + exp(-steepness * (temp - midpoint)));
-    return power;
+    return calculatePower(49.1, 0.078, 18.0, getLastTemp());
 }
 
 float updateFanAutoHighPower()
 {
-    float temp = getLastTemp();
-    int midpoint = 33;
-    float steepness = 0.4;
-    float power = 1 / (1 + exp(-steepness * (temp - midpoint)));
-    return power;
+    return calculatePower(62.55, 0.09626, 18.0, getLastTemp());
 }
